a468587192b4c845471c6884343cf2b005ecc385
Julia Di Monte <dimontejulia@gmail.com>
2020-10-08 20:20:13 +0000

    array practice

---
+++ ice-cream.js
@@ -0,0 +1,19 @@
+// Do not modify these first two lines
+let iceCreamFlavours = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry",
+];
+console.log(iceCreamFlavours);
+
+// step1
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+// step2
+console.log(iceCreamFlavours[0]);
+//step3
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+//step4
+console.log(iceCreamFlavours.length);
